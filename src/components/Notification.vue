9a6ad01c6188be65c859514a3a91a847e2ea5839
Loic Chazeau <loic.chazeau@epitech.digital>
2021-05-04 17:48:58 +0200

    add navbar components

---
+++ src/components/Notification.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    id="icon_social_notifications_none_24px"
+    data-name="icon/social/notifications_none_24px"
+    xmlns="http://www.w3.org/2000/svg"
+    width="31"
+    height="31"
+    viewBox="0 0 31 31"
+  >
+    <rect id="Boundary" width="31" height="31" fill="none" />
+    <path
+      id="_Color"
+      data-name=" ↳Color"
+      d="M11.5,26.5a2.8,2.8,0,0,1-2.875-2.719h5.749A2.8,2.8,0,0,1,11.5,26.5ZM23,22.422H0V21.064l2.875-2.718v-6.8A9.254,9.254,0,0,1,4.567,6.034,7.974,7.974,0,0,1,9.343,2.962V2.039a2.16,2.16,0,0,1,4.313,0v.923a7.963,7.963,0,0,1,4.781,3.076,9.262,9.262,0,0,1,1.688,5.512v6.8L23,21.064v1.357ZM11.5,5.435A5.479,5.479,0,0,0,7.282,7.229a6.439,6.439,0,0,0-1.533,4.321V19.7h11.5V11.551a6.44,6.44,0,0,0-1.532-4.321A5.477,5.477,0,0,0,11.5,5.435Z"
+      transform="translate(4 2)"
+      fill="#3e779f"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "Notification",
+};
+</script>
